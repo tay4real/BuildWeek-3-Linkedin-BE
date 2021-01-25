d7d47e55c8c8858336580a6dd62cc570413d0198
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-01-25 16:47:06 +0100

    CRUD for users

--- src/services/profile/index.js
+++ src/services/profile/index.js
@@ -1,7 +1,87 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const profileSchema = require("./profileSchema");
+const PDFDocument = require("pdfkit");
+const blobStream  = require('blob-stream');
+const { createWriteStream } = require("fs-extra")
 
-router.get('/', async(req,res,next)=> {
-    res.send('ok')
-})
+router.get("/", async (req, res, next) => {
+  try {
+    const profile = await profileSchema.find();
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+});
 
-module.exports = router
+router.get("/:id", async (req, res, next) => {
+  try {
+    const profile = await profileSchema.findById(req.params.id);
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+});
+// router.get("/:id/cv", async (req, res, next) => {
+    
+    
+//   try {
+//     const profile = await profileSchema.findById(req.params.id);
+//     const doc = new PDFDocument();
+//     doc.pipe(res)
+//     let stream = doc.pipe(blobStream());
+//     doc.pipe(await createWriteStream("output.pdf"));
+//     doc.text(`Name: ${profile.name}`, 100, 100);
+//     doc.end();
+//     stream.on('finish', function() {
+//         iframe.src = stream.toBlob('application/pdf');
+//       });
+//     res.send("PDF created")
+//   } catch (error) {
+//     next(error);
+//   }
+// });
+/* 
+{
+        "_id": "5d84937322b7b54d848eb41b", //server generated
+        "name": "Diego",
+        "surname": "Banovaz",
+        "email": "diego@strive.school",
+        "bio": "SW ENG",
+        "title": "COO @ Strive School",
+        "area": "Berlin",
+        "image": ..., //server generated on upload, set a default here
+        "username": "admin",
+        "createdAt": "2019-09-20T08:53:07.094Z", //server generated
+        "updatedAt": "2019-09-20T09:00:46.977Z", //server generated
+    }
+*/
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newUser = new profileSchema(req.body);
+    const { _id } = await newUser.save();
+
+    res.status(201).send(newUser);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const profile = await profileSchema.findByIdAndUpdate(
+      req.params.id, //this is what to look for
+      req.body, //this is what to update it to
+      {
+        runValidators: true,
+        returnOriginal: false,
+        useFindAndModify: false,
+      }
+    );
+    res.send(profile);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
---
+++ src/services/profile/profileSchema.js
@@ -0,0 +1,17 @@
+const { Schema } = require("mongoose")
+const mongoose = require("mongoose")
+
+const profileSchema = new Schema(
+    {
+        "name": String,
+        "surname": String,
+        "email": String,
+        "bio": String,
+        "title": String,
+        "area": String,
+        "username": String
+    },
+  { timestamps: true }
+)
+
+module.exports = mongoose.model("profile", profileSchema)
