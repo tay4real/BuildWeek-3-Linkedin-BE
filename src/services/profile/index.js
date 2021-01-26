92f412dd904284bebfc1a4f288aa0f08a03aa5fe
Lidia Kovac <lidia.kovac1998@gmail.com>
2021-01-26 09:48:56 +0100

    adds image upload

--- src/services/profile/index.js
+++ src/services/profile/index.js
@@ -1,9 +1,28 @@
 const router = require("express").Router();
 const profileSchema = require("./profileSchema");
 const PDFDocument = require("pdfkit");
-const blobStream  = require('blob-stream');
-const { createWriteStream } = require("fs-extra")
-const fs = require('fs');
+const blobStream = require("blob-stream");
+const { createWriteStream } = require("fs-extra");
+const fs = require("fs");
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinary = require("./cloudinary_setup");
+const { findOneAndUpdate } = require("./profileSchema");
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "propics"
+  },
+});
+/* var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, '/tmp/my-uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})*/
+const cloudinaryMulter = multer({ storage: storage });
 
 router.get("/", async (req, res, next) => {
   try {
@@ -23,45 +42,32 @@ router.get("/:id", async (req, res, next) => {
   }
 });
 router.get("/:id/cv", async (req, res, next) => {
-    
-    
   try {
     const profile = await profileSchema.findById(req.params.id);
     let doc = new PDFDocument();
     doc.pipe(res);
-    
-    doc.text(`
+
+    doc.text(
+      `
     Name: ${profile.name},
     Surname: ${profile.surname}, 
     email: ${profile.email},
     biography: ${profile.bio} primarily in ${profile.area},
     title: ${profile.title},
     Experience: ${"Here will be the experience"}
-     `, 100, 100);
+     `,
+      100,
+      100
+    );
     doc.end();
     await res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-      });
-      res.status(201).send("OK")
+      "Content-Type": "application/pdf",
+    });
+    res.status(201).send("OK");
   } catch (error) {
     next(error);
   }
 });
-/* 
-{
-        "_id": "5d84937322b7b54d848eb41b", //server generated
-        "name": "Diego",
-        "surname": "Banovaz",
-        "email": "diego@strive.school",
-        "bio": "SW ENG",
-        "title": "COO @ Strive School",
-        "area": "Berlin",
-        "image": ..., //server generated on upload, set a default here
-        "username": "admin",
-        "createdAt": "2019-09-20T08:53:07.094Z", //server generated
-        "updatedAt": "2019-09-20T09:00:46.977Z", //server generated
-    }
-*/
 
 router.post("/", async (req, res, next) => {
   try {
@@ -73,6 +79,26 @@ router.post("/", async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/:id/propic", cloudinaryMulter.single('image'), async (req, res, next) => {
+  try {
+    let img_path = await req.file.path
+    console.log("IMAGE STORED IN : ", img_path)
+    //const profile = await profileSchema.find({_id: req.params.id})
+    await profileSchema.findByIdAndUpdate(req.params.id, {image: img_path},
+      {
+        runValidators: true,
+        returnOriginal: false,
+        useFindAndModify: false,
+      }
+      )
+    res.send(img_path)
+  } catch (error) {
+    next(error);
+  }
+});
+
+
 
 router.put("/:id", async (req, res, next) => {
   try {
